aa34584ae7ffd595943a43b6c560f6bf85e7a0e9
Daniel Gonzalez <gonzalezvaldepenadaniel@gmail.com>
2025-12-30 11:26:02 -0600

    correcion ventana emergente

--- Administrador/usuario.js
+++ Administrador/usuario.js
@@ -1,12 +1,55 @@
 console.log("usuarios.js cargado");
 
-function nuevoUsuario(){
-    $("#formUsuario")[0].reset();
-    $("#usu_id").val("");
-    $("#modalUsuario").modal("show");
+/* ==========================
+   FUNCIÓN LIMPIAR MODALES
+========================== */
+function limpiarModales() {
+    $(".modal").modal("hide");
+    $(".modal-backdrop").remove();
+    $("body").removeClass("modal-open");
 }
 
-// GUARDAR / EDITAR
+/* ==========================
+   NUEVO USUARIO
+========================== */
+function nuevoUsuario(){
+
+    limpiarModales();
+
+    setTimeout(function(){
+        $("#formUsuario")[0].reset();
+        $("#usu_id").val("");
+        $("#modalUsuario").modal({
+            backdrop: "static",
+            keyboard: false
+        });
+    }, 300);
+}
+
+/* ==========================
+   EDITAR USUARIO
+========================== */
+$(document).on("click", ".editar", function(){
+
+    limpiarModales();
+
+    setTimeout(() => {
+        $("#usu_id").val($(this).data("id"));
+        $("#usu_nombre").val($(this).data("nombre"));
+        $("#usu_apellido").val($(this).data("apellido"));
+        $("#usu_correo").val($(this).data("correo"));
+        $("#rol").val($(this).data("rol"));
+
+        $("#modalUsuario").modal({
+            backdrop: "static",
+            keyboard: false
+        });
+    }, 300);
+});
+
+/* ==========================
+   GUARDAR / EDITAR
+========================== */
 $("#formUsuario").on("submit", function(e){
     e.preventDefault();
 
@@ -15,18 +58,9 @@ $("#formUsuario").on("submit", function(e){
     });
 });
 
-// EDITAR
-$(document).on("click", ".editar", function(){
-    $("#usu_id").val($(this).data("id"));
-    $("#usu_nombre").val($(this).data("nombre"));
-    $("#usu_apellido").val($(this).data("apellido"));
-    $("#usu_correo").val($(this).data("correo"));
-    $("#rol").val($(this).data("rol"));
-
-    $("#modalUsuario").modal("show");
-});
-
-// ELIMINAR
+/* ==========================
+   ELIMINAR
+========================== */
 $(document).on("click", ".eliminar", function(){
     if(confirm("¿Eliminar usuario?")){
         $.post("creaedit.php", {
@@ -36,4 +70,12 @@ $(document).on("click", ".eliminar", function(){
             location.reload();
         });
     }
+});
+
+/* ==========================
+   AL CERRAR FORMULARIO
+========================== */
+$("#modalUsuario").on("hidden.bs.modal", function(){
+    limpiarModales();
+    $("#modalUsuarios").modal("show");
 });
